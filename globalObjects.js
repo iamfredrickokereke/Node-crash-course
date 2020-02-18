63cbb8dd4fca98ca5d28ab593b23a8d21ca2ff03
Kelly online <kellyonline2016@gmail.com>
2020-02-18 01:36:16 +0100

    update file name and added some lines

---
+++ globalObjects.js
@@ -0,0 +1,29 @@
+
+
+// how setTimeout works
+// 
+// setTimeout( () => {
+//     console.log("3 seconds has passed!")
+// }, 1000)
+
+
+//loops on interval setted
+
+// let time = 0
+
+// let timer = setInterval(() => {
+//     time+=2
+//     console.log(time + " seconds also passed")
+
+//     if (time > 6){
+
+//         console.log("exit...")
+//         clearInterval(timer)
+       
+//     }
+// }, 3000);
+
+
+console.log(__dirname)
+
+console.log(__filename)
