3b2cc8773f3d17f73bc3aec61b640c62e429fce9
Kelly online <kellyonline2016@gmail.com>
2020-02-19 00:00:37 +0100

    add named and anonymous functions

---
+++ functions.js
@@ -0,0 +1,21 @@
+// a normal named function 
+
+function sum(a,b) {
+    return a + b
+}
+
+
+result = sum(4,10)
+
+console.log(result)
+
+
+// using an anonymous function expression
+
+let name = function (name) {
+    return name
+}
+
+result1 = name("fred")
+
+console.log(result1)
