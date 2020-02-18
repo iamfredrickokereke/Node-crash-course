51ff90a49f16a8c306c50a620b54c24a878df41f
Kelly online <kellyonline2016@gmail.com>
2020-02-18 22:02:41 +0100

    fix typo and indentations

--- globalObjects.js
+++ globalObjects.js
@@ -1,27 +1,30 @@
+//Example of global objects in Node,  which some where window objects in the browser.
+
+// require, exports, __dirname, __filename
 
 
 // how setTimeout works
-// 
-// setTimeout( () => {
-//     console.log("3 seconds has passed!")
-// }, 1000)
+
+setTimeout( () => {
+    console.log("3 seconds has passed!")
+}, 1000)
 
 
 //loops on interval setted
 
-// let time = 0
+let time = 0
 
-// let timer = setInterval(() => {
-//     time+=2
-//     console.log(time + " seconds also passed")
+let timer = setInterval(() => {
+    time+=2
+    console.log(time + " seconds also passed")
 
-//     if (time > 6){
+    if (time > 6){
 
-//         console.log("exit...")
-//         clearInterval(timer)
+        console.log("exit...")
+        clearInterval(timer)
        
-//     }
-// }, 3000);
+    }
+}, 3000);
 
 
 console.log(__dirname)
